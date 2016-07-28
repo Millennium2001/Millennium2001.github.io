58b142598d4858e9241535cf7ba7eb9e7897f7d3
SnO2WMaN <numaguchisow@gmail.com>
2016-07-29 00:50:50 +0900

    Fixed Path 3.

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -1,58 +1,58 @@
 $(function () {
-    var $window = $(window)
-        , $body = $("body")
-        , $menu = $("#menu")
-        , $items = $("<ul>");
-    var items = [{
-        title: "HOME"
-        , href: "/index.html"
+  var $window = $(window)
+    , $body = $("body")
+    , $menu = $("#menu")
+    , $items = $("<ul>");
+  var items = [{
+    title: "HOME"
+    , href: "https://sno2wman.github.io/MillenniumRecords-Website/index.html"
     }, {
-        title: "ALBUMS"
-        , href: "/albums/index.html"
+    title: "ALBUMS"
+    , href: "https://sno2wman.github.io/MillenniumRecords-Website/albums/index.html"
     }, {
-        title: "MEMBERS"
-        , href: "/members/index.html"
+    title: "MEMBERS"
+    , href: "https://sno2wman.github.io/MillenniumRecords-Website/members/index.html"
     }, {
-        title: "JOIN"
-        , href: "/join.html"
+    title: "JOIN"
+    , href: "https://sno2wman.github.io/MillenniumRecords-Website/join.html"
     }, {
-        title: "CONTACT"
-        , href: "/contact.html"
+    title: "CONTACT"
+    , href: "https://sno2wman.github.io/MillenniumRecords-Website/contact.html"
     }];
-    for (var item in items) {
-        mkItem($items, items[item]);
-    }
+  for (var item in items) {
+    mkItem($items, items[item]);
+  }
 
-    function mkItem(root, object, index) {
-        var $root = $(root);
-        var $item = $("<li>").css("transition-delay", index * 0.05 + "s");
-        var $title = $("<h1>", {
-            text: object.title
-        });
-        var $link = $("<a>", {
-            href: object.href
-            , class: "fill_link"
-        });
-        var $childrenBox = $("<ul>");
-        $.each(object.children, function (index, c) {
-            mkItem($childrenBox, c, index);
-        })
-        $root.append($item.append($link, $title, $childrenBox));
-    }
-    $menu.append($items);
-    if (!$menu.hasClass("nofix")) {
-        var firstTop = $menu.offset().top
-            , menuH = $menu.height();
-        $window.on("scroll", function () {
-            var scr = $(this).scrollTop();
-            if (firstTop <= scr) {
-                $menu.addClass("sticky");
-                $body.css("margin-top", menuH);
-            }
-            else {
-                $menu.removeClass("sticky");
-                $body.css("margin-top", 0);
-            }
-        }).trigger("scroll");
-    }
+  function mkItem(root, object, index) {
+    var $root = $(root);
+    var $item = $("<li>").css("transition-delay", index * 0.05 + "s");
+    var $title = $("<h1>", {
+      text: object.title
+    });
+    var $link = $("<a>", {
+      href: object.href
+      , class: "fill_link"
+    });
+    var $childrenBox = $("<ul>");
+    $.each(object.children, function (index, c) {
+      mkItem($childrenBox, c, index);
+    })
+    $root.append($item.append($link, $title, $childrenBox));
+  }
+  $menu.append($items);
+  if (!$menu.hasClass("nofix")) {
+    var firstTop = $menu.offset().top
+      , menuH = $menu.height();
+    $window.on("scroll", function () {
+      var scr = $(this).scrollTop();
+      if (firstTop <= scr) {
+        $menu.addClass("sticky");
+        $body.css("margin-top", menuH);
+      }
+      else {
+        $menu.removeClass("sticky");
+        $body.css("margin-top", 0);
+      }
+    }).trigger("scroll");
+  }
 });
